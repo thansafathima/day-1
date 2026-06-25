1365ed8685ec48db68736dfacdfad3f67339873f
thansafathima <thansafathimaa@gmail.com>
2026-06-25 17:42:47 +0530

    Add files via upload

---
+++ five.py
@@ -0,0 +1,73 @@
+#comparison operators
+
+# a = 10
+# b = 20
+# #equal to
+# print (a == b)
+
+
+#not equal to
+# a = 10
+# b = 20
+# print (a != b)
+
+# #greater than
+# a = 10
+# b = 20
+# # print (a > b)
+
+##less than or equal to
+# a = 10
+# b = 20
+# print (a <= b)
+
+
+# conitional statements
+
+
+#  if condition:
+#     # code to be executed
+
+
+# if 
+# if else
+# elif
+
+
+
+# 
+#check if a number is odd or even
+# no = 19
+# if no % 2 == 0:
+#     print("even")
+# else:
+#     print('odd')
+
+
+# 
+# check if a number is multiple of 5
+
+# no = 30
+# if no % 5 == 0:
+#     print("multiple of 5")
+# else:
+#     print("not a multiple of 5")
+
+# largest of 2 numbers
+# a = 5
+# b = 7
+# if a > b:
+#     print(a)
+# else:
+#     print(b)
+
+
+# check if a number is positive,negative or zero
+
+num = 0
+if num > 0:
+    print("it is positive number")
+elif num < 0:
+    print("it is negative number")
+else:
+    print ("it is zero")
