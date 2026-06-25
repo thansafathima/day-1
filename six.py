40c2e89288d8501c38d3ff926e7c72f9795a0f22
thansafathima <thansafathimaa@gmail.com>
2026-06-25 17:43:11 +0530

    Add files via upload

---
+++ six.py
@@ -0,0 +1,24 @@
+# logical operators
+
+# and
+# or
+# not
+
+# and
+# true and true.  True
+# true and false  False
+# false and true  False
+# false and false false
+
+# 
+
+# not
+# a = 10
+# print(not a == 10)
+
+# largest of 3 numbers
+
+# check if a number is multiple of 3 and 5
+num = 30
+if (num % 5 ==0 and num % 3==0):
+    print("it is a multiple of 3 and 5")
