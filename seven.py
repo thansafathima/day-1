3bd32cd119e851f19b8e7de48d376143e961fa72
thansafathima <thansafathimaa@gmail.com>
2026-06-28 21:23:28 +0530

    Add files via upload

---
+++ seven.py
@@ -0,0 +1,76 @@
+# string.   
+# sequence of series
+
+# name = "mohan"
+# print(name)
+# print(type(name))
+
+# qt ='''gandhi once said that"say no to violance"'''
+# print(qt)
+
+
+# Escape sequence
+
+# \n new line
+# \t tab space
+# \b back space
+# \' --  '
+# \" -- "
+
+
+# data = 'mohan\'s resume'
+# print (data)
+
+# qt = "gandhi\b on\t ce said \n that \"say no to violance\" "
+# print(qt)
+
+#double slash indel \n can be \n
+
+# Raw string 
+# --------------
+# qt = r"gandhi\b on\t ce said \n that \"say no to violance\" "
+# print(qt)
+# to get as raw..like that we type,simply put 'r' before the 
+
+
+# input function
+# ----------------
+
+# a = input("enter a number-")
+# b = input ("enter another number-")
+# print(a+b)
+
+# string can only be addded with string not with integers
+
+# a = input("enter a number-")
+# b = input ("enter another number-")
+# print(a+' '+b)
+
+# TYPE CONVERSION/CASTING
+# ------------------------
+# Implicit
+# explicit
+
+# a = "10"
+# b = "21"
+# print(int(a)+int(b))
+
+
+
+# a = float(input("enter a number"))
+# b = float(input("enter another number"))
+# print(a+b)
+
+
+# STRING FORMAT
+# --------------
+
+# name = "mohan"
+# age = 29
+# married_status = False
+# rating = 4.9
+
+# result = f"my name is {name},my age is {age},my married_status is {married_status},my rating is{rating}"
+# print(result)
+
+# put "f" before it
