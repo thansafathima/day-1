e4adfee6bdee1993404171162b00cdbbb6e3baf5
thansafathima <thansafathimaa@gmail.com>
2026-06-28 21:23:58 +0530

    Add files via upload

---
+++ eight.py
@@ -0,0 +1,63 @@
+# Control statements
+
+# loops
+
+# while
+# loop
+
+# while
+# condition based loop
+
+
+# while condition:
+#     code to be executed
+
+# while True:
+#     print("mohan")
+
+# if there is an infinite loop click on the terminal and ctrl+c
+
+
+# i = 1
+# while i < 6:
+#     print(i)
+#     i = i + 1
+# 
+# 
+# i = 1
+# while i <= 100:
+#     i = i + 1
+# print(i) 
+
+
+#sum of first 10 numbers
+
+# i = 1
+# sum = 0
+# while i <= 10:
+#     sum = sum + i
+#     i = i + 1
+# print(sum)
+
+
+# i = 1
+# sum = 0
+# while i <= 15:
+#     i = i + 1
+#     sum = sum + i
+# print (sum)
+
+
+
+# print even sum and odd sum from first 100 numbers
+# i = 1
+# sum1 = 0
+# sum2 = 0
+# while i <= 100
+#     if i % 2 == 0:
+#         sum2 = sum2 + i
+#     else:
+#         sum1 = sum1 + i
+#     i = i + 1
+# print(sum1)
+# print(sum2)    
