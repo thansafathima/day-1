e2b75d0544b12b02df7638cb44e3b29ccda42328
thansafathima <thansafathimaa@gmail.com>
2026-06-29 21:31:11 +0530

    Add files via upload

---
+++ nine.py
@@ -0,0 +1,78 @@
+# sum of digits in a number
+
+# num = 12345
+# sum = 0
+# while num > 0:
+#     b = num % 10
+#     sum = sum + b
+#     num = num // 10
+# print(sum)
+
+
+# REVERSE OF A NUMBER
+
+# num = 543
+# rev = 0
+# while num > 0:
+#     b = num % 10
+#     rev = rev * 10 + b
+#     num = num // 10
+# print(rev)
+
+
+# fibonacci series
+
+
+# factorial
+
+# num = int(input("enter a number"))
+# fact = 1
+# while num > 0:
+#     fact = fact * num
+#     num = num - 1
+# print(fact)
+
+
+
+# for loop
+# ----------
+
+# for i in range:
+    # code to be executed
+
+# range(start,stop,step)
+
+# for i in range(2,7,2):
+#     print(i)
+
+# for i in range(10,1,-1):
+#     print(i)
+
+#100 numbers sum
+# ------------------
+# i = 0
+# sum = 0
+# for i in range(101):
+#     sum = sum + i
+# print(sum)
+
+# Factorial of a number
+# ---------------------
+
+# num = int (input("enter a num"))
+# fact = 1
+
+# for i in range(1,num + 1):
+#     fact = fact * i
+    
+# print(fact)
+
+
+# multiplication table of a number
+# # ---------------------------------
+# num = 5
+# for i in range(1,11):
+#     print(f'{num} * {i} = {num * i}')
+
+# sum and average of n numbers
+
