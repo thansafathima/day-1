367d1f45b99b111f7db4855ef43417c24316fe4c
thansafathima <thansafathimaa@gmail.com>
2026-07-01 21:16:18 +0530

    Add files via upload

---
+++ thirteen.py
@@ -0,0 +1,69 @@
+# 24-06-2026
+# nested loops
+# for i in range(1,6):
+#     for j in range(1,6):
+#         print(i,j)
+
+# for i in range(1,6):
+#     for j in range(1,6):
+#         for k in range(1,6):
+#             print(i,j)
+
+
+# patterns
+
+# *
+# **
+# ***
+# ****
+# *****      print this
+
+
+# for i in range(1, 6):
+#     for j in range(1, i+1):
+#         print("*", end="")   
+#     print()
+
+# # reverse of the stars
+# for i in range(5,0,-1):
+#     for j in range(1,i+1):
+#         print("*",end ="")
+#     print()
+
+
+# equilatoral triangle
+#      *
+#     * *
+#    * * *
+#   * * * *
+#  * * * * *
+
+# for i in range(1,6):
+#     for j in range(6-i):
+#         print(" ",end="")
+#     for k in range(1,i+1):
+#         print("* ",end="")
+#     print()
+
+# print chessboard pattern
+
+# W B W B W B W B
+# W B W B W B W B
+# W B W B W B W B
+# W B W B W B W B
+# W B W B W B W B
+# W B W B W B W B
+# W B W B W B W B
+# W B W B W B W B
+
+
+# for i in range(1,9):
+#     for j in range(1,9):
+#         if (i+j) % 2 == 0 :
+#             print("W",end=" ")
+#         else:
+#             print("B",end=" ")
+#     print()
+
+
+list a 100 python mcq questionswith context data types,operators,loops and conditional statements
