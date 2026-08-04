90a7d09eaa8389728f2ef5992ff8dc6a56c9c555
thansafathima <thansafathimaa@gmail.com>
2026-08-04 21:51:11 +0530

    Add files via upload

---
+++ fifteen.py
@@ -0,0 +1,116 @@
+# 30-06-2026
+
+# hollow square
+
+# n = int(input("Enter the size: "))
+
+# for i in range(n):
+#     for j in range(n):
+#         if i == 0 or i == n-1 or j == 0 or j == n-1:
+#             print("*", end=" ")
+#         else:
+#             print(" ", end=" ")
+#     print()
+
+
+
+
+# print H
+# ===========
+# i=Row
+# j=Coloumn
+
+# for i in range(1,6):
+#     for j in range(1,6):
+#         if j==1 or j==5 or i ==3:
+#             print("*",end="")
+#         else:
+#             print(" ",end= "")
+#     print()
+
+
+# printI
+# ======
+
+# for i in range(1,6):
+#     for j in range(1,6):
+#         if i==1 or i==5 or j ==3:
+#             print("*",end="")
+#         else:
+#             print(" ",end= "")
+#     print()
+
+
+# print S 
+# ========
+
+
+
+
+
+
+
+# Return Statement
+
+# def add2(a,b):
+#     return 1,"mohan",True
+# print(add2(1,3))
+
+
+# scope
+
+# Create a calculator using functions
+ 
+# def add(a,b):
+#     return a+b
+# def sub(a,b):
+#     return a-b
+# def mul(a,b):
+#     return a*b
+# def div(a,b):
+#     return a/b
+# def main():
+#     while True:
+#         print("welcome to simple Calculator")
+#         ch = int(input("enter your choice \n1.Add\n2.Sub\n3.MUl\n4.div\n5.exit\n..."))
+#         x = int(input("enter number:-"))
+#         y = int(input("enter number:-"))
+#         if ch == 1:
+#             print(add(x,y))
+#         elif ch == 2:
+#             print(sub(x,y))
+#         elif ch == 3:
+#             print(mul(x,y))
+#         elif ch == 4:
+#             print(div(x,y))
+#         elif ch == 5:
+#             break
+#         else:
+#             print("Invalid Choice")
+# main()
+
+
+# lambda functions in new folder....lambda.py
+
+
+# revision
+
+# sum of odd and even numbers    
+
+# i = 1
+# odd = 0
+# even = 0
+# while i <= 100:
+#     if i % 2 == 0:
+#         even = even + i
+#     else:
+#         odd = odd + i
+#     i = i + 1
+# print(odd)
+# print(even)
+
+# sum of digits in a no
+
+# num = 12345
+# sum = 0
+
