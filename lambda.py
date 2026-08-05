aad49073b38fa07d534867a3d540ddc933f4da2e
thansafathima <thansafathimaa@gmail.com>
2026-08-05 13:50:33 +0530

    Add files via upload

---
+++ lambda.py
@@ -0,0 +1,62 @@
+# lambda function
+# it is an anonymous function
+# lambda arguments : expression
+# it is a single line function
+
+# # eg:
+# def add2(a,b):
+#     return(a+b)
+# print(add2(5,4))
+#  we can write this in lambda expression like,
+
+# z = lambda x,y : x+y
+# print(z(2,3))
+
+# product of 3 numbers
+# z = lambda x,y,z : x*y*z
+# print(z(1,2,4))
+
+# square of a number
+# sqr = lambda x : x**2
+# print(sqr(3))
+
+# perimeter of circle
+# peri = lambda radius : 2 * 3.14 * radius
+
+# area of triangle
+# area = lambda base,height : (base*height)/5
+
+
+# average of 5 numbers
+# avg = lambda a,b,c,d,e:(a+b+c+d+e)/5
+
+# square root of a number
+
+# sqrt = lambda x : x**0.5
+
+# full name of a person
+
+# fullname = lambda fname,mname,lname : fname+" "+lname
+
+
+
+# # ternary operator 
+# it is for sinle if and else
+# check if a person is eligible for vote or not
+
+# print ("eligible") if age >= 18 else print("not eligible")
+
+
+# it in lambda function,
+
+# check = lambda age : "eligible" if age >= 18 else "not eligible"
+# print(check(21))
+
+
+
+# pep 8
+# standard format
+
+
+# age calculator
+# bmi calculator
